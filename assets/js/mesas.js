f13991924aa90c76674622fd2412578f117b5855
Heder John de Jesus Silva <hedersi@gmail.com>
2021-06-09 09:35:19 -0300

    Ultimas alterações

---
+++ assets/js/mesas.js
@@ -0,0 +1,86 @@
+var result = [
+	{
+		"Id": 1,
+		"Descricao": "Mesa 01",
+		"X": 205,
+		"Y": 32,
+		"Ativo": false
+	},
+	{
+		"Id": 2,
+		"Descricao": "Mesa 02",
+		"X": 205,
+		"Y": 75,
+		"Ativo": true
+	},
+	{
+		"Id": 3,
+		"Descricao": "Mesa 03",
+		"X": 205,
+		"Y": 117,
+		"Ativo": false
+	},
+	{
+		"Id": 4,
+		"Descricao": "Mesa 04",
+		"X": 263,
+		"Y": 32,
+		"Ativo": true
+	},
+	{
+		"Id": 5,
+		"Descricao": "Mesa 05",
+		"X": 263,
+		"Y": 75,
+		"Ativo": false
+	},
+	{
+		"Id": 6,
+		"Descricao": "Mesa 03",
+		"X": 263,
+		"Y": 117,
+		"Ativo": true
+	},
+	{
+		"Id": 7,
+		"Descricao": "Mesa 01",
+		"X": 115,
+		"Y": 32,
+		"Ativo": false
+	},
+	{
+		"Id": 8,
+		"Descricao": "Mesa 02",
+		"X": 115,
+		"Y": 75,
+		"Ativo": true
+	},
+	{
+		"Id": 9,
+		"Descricao": "Mesa 03",
+		"X": 115,
+		"Y": 117,
+		"Ativo": false
+	},
+	{
+		"Id": 10,
+		"Descricao": "Mesa 04",
+		"X": 170,
+		"Y": 32,
+		"Ativo": true
+	},
+	{
+		"Id": 11,
+		"Descricao": "Mesa 05",
+		"X": 170,
+		"Y": 75,
+		"Ativo": false
+	},
+	{
+		"Id": 12,
+		"Descricao": "Mesa 03",
+		"X": 170,
+		"Y": 117,
+		"Ativo": true
+	}
+]
---
+++ assets/js/mesas_reserva.js
@@ -0,0 +1,146 @@
+var result = [
+	{
+		"Id": 1,
+		"Descricao": "Mesa 01",
+		"X": 205,
+		"Y": 32,
+		"Disponivel": false
+	},
+	{
+		"Id": 2,
+		"Descricao": "Mesa 02",
+		"X": 205,
+		"Y": 75,
+		"Disponivel": false,
+		"Reserva": {
+			"IdReserva": 10,
+			"Nome": "João",
+			"IdUsuario": 10
+		}
+	},
+	{
+		"Id": 3,
+		"Descricao": "Mesa 03",
+		"X": 205,
+		"Y": 117,
+		"Disponivel": false
+	},
+	{
+		"Id": 4,
+		"Descricao": "Mesa 04",
+		"X": 263,
+		"Y": 32,
+		"Disponivel": true
+	},
+	{
+		"Id": 5,
+		"Descricao": "Mesa 05",
+		"X": 263,
+		"Y": 75,
+		"Disponivel": false
+	},
+	{
+		"Id": 6,
+		"Descricao": "Mesa 03",
+		"X": 263,
+		"Y": 117,
+		"Disponivel": true
+	},
+	{
+		"Id": 7,
+		"Descricao": "Mesa 01",
+		"X": 115,
+		"Y": 32,
+		"Disponivel": false
+	},
+	{
+		"Id": 8,
+		"Descricao": "Mesa 02",
+		"X": 115,
+		"Y": 75,
+		"Disponivel": true
+	},
+	{
+		"Id": 9,
+		"Descricao": "Mesa 03",
+		"X": 115,
+		"Y": 117,
+		"Disponivel": false
+	},
+	{
+		"Id": 10,
+		"Descricao": "Mesa 04",
+		"X": 170,
+		"Y": 32,
+		"Disponivel": true
+	},
+	{
+		"Id": 11,
+		"Descricao": "Mesa 05",
+		"X": 170,
+		"Y": 75,
+		"Disponivel": false
+	},
+	{
+		"Id": 12,
+		"Descricao": "Mesa 17",
+		"X": 170,
+		"Y": 117,
+		"Disponivel": false,
+		"Reserva": {
+			"IdReserva": 11,
+			"Nome": "Leonador",
+			"IdUsuario": 15
+		}
+	},
+	{
+		"Id": 12,
+		"Descricao": "Mesa 03",
+		"X": 115,
+		"Y": 173,
+		"Disponivel": false,
+		"Reserva": {
+			"IdReserva": 11,
+			"Nome": "João",
+			"IdUsuario": 15
+		}
+	},
+	{
+		"Id": 12,
+		"Descricao": "Mesa 03",
+		"X": 115,
+		"Y": 233,
+		"Disponivel": false,
+		"Reserva": {
+			"IdReserva": 11,
+			"Nome": "João",
+			"IdUsuario": 15
+		}
+	},
+	{
+		"Id": 12,
+		"Descricao": "Mesa 03",
+		"X": 170,
+		"Y": 173,
+		"Disponivel": false,
+		"Reserva": {
+			"IdReserva": 11,
+			"Nome": "Tassio Seiji",
+			"IdUsuario": 15,
+			"Imagem": "assets/img/usuario_tassio.png"
+		}
+	},
+	{
+		"Id": 12,
+		"Descricao": "Mesa 03",
+		"X": 170,
+		"Y": 222,
+		"Disponivel": false,
+		"Reserva": {
+			"IdReserva": 11,
+			"Nome": "Gustavo Lopes",
+			"IdUsuario": 15,
+			"Imagem": "assets/img/usuario_gus.png"
+		}
+	}
+]
